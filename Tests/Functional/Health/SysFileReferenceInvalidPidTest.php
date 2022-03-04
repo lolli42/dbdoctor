bb553a28868067d0b993627b8da819afdddfeb0c
Christian Kuhn <lolli@schwarzbu.ch>
2022-03-04 16:12:25 +0100

    [TASK] Testing SysFileReferenceInvalidPid

---
+++ Tests/Functional/Health/SysFileReferenceInvalidPidTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lolli\Dbhealth\Tests\Functional\Health;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Lolli\Dbhealth\Health\HealthInterface;
+use Lolli\Dbhealth\Health\SysFileReferenceInvalidPid;
+use Lolli\Dbhealth\Health\SysFileReferenceInvalidTableLocal;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+class SysFileReferenceInvalidPidTest extends FunctionalTestCase
+{
+    use ProphecyTrait;
+
+    protected array $testExtensionsToLoad = [
+        'typo3conf/ext/dbhealth',
+    ];
+
+    /**
+     * @test
+     */
+    public function fixBrokenRecords(): void
+    {
+        $this->importCSVDataSet(__DIR__ . '/../Fixtures/SysFileReferenceInvalidPidImport.csv');
+        $io = $this->prophesize(SymfonyStyle::class);
+        $io->ask(Argument::cetera())->willReturn('e');
+        /** @var SysFileReferenceInvalidPid $subject */
+        $subject = $this->get(SysFileReferenceInvalidPid::class);
+        $subject->handle($io->reveal(), HealthInterface::MODE_EXECUTE, '');
+        $io->warning(Argument::cetera())->shouldHaveBeenCalled();
+        $io->note(Argument::cetera())->shouldHaveBeenCalled();
+        $io->text(Argument::cetera())->shouldHaveBeenCalled();
+        $this->assertCSVDataSet(__DIR__ . '/../Fixtures/SysFileReferenceInvalidPidFixed.csv');
+    }
+}
