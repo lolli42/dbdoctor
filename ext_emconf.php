b9ba4dc78b634de4918d0652057c9f0997d657f7
Christian Kuhn <lolli@schwarzbu.ch>
2022-03-23 19:49:01 +0100

    [RELEASE] 0.1.2 First TER release

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF['dbdoctor'] = [
     'title' => 'TYPO3 Database doctor',
     'description' => 'Find and fix TYPO3 database inconsistencies',
     'category' => 'misc',
-    'version' => '0.1.1',
+    'version' => '0.1.2',
     'module' => '',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
@@ -12,7 +12,7 @@ $EM_CONF['dbdoctor'] = [
     'author_email' => 'lolli@schwarzbu.ch',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.0.99',
+            'typo3' => '11.5.0-11.99.99',
         ],
         'conflicts' => [
         ],
