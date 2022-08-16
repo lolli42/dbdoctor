dd232d94b51fe5044558fe321909806cad610c07
Christian Kuhn <lolli@schwarzbu.ch>
2022-08-16 14:53:04 +0200

    [TASK] Add a series of language related checks (#6)

---
+++ Tests/Functional/Health/SysFileReferenceLocalizedParentDeletedTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lolli\Dbdoctor\Tests\Functional\Health;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Lolli\Dbdoctor\Health\HealthInterface;
+use Lolli\Dbdoctor\Health\SysFileReferenceLocalizedParentDeleted;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+class SysFileReferenceLocalizedParentDeletedTest extends FunctionalTestCase
+{
+    use ProphecyTrait;
+
+    protected array $testExtensionsToLoad = [
+        'typo3conf/ext/dbdoctor',
+    ];
+
+    /**
+     * @test
+     */
+    public function fixBrokenRecords(): void
+    {
+        $this->importCSVDataSet(__DIR__ . '/../Fixtures/SysFileReferenceLocalizedParentDeletedImport.csv');
+        $io = $this->prophesize(SymfonyStyle::class);
+        $io->ask(Argument::cetera())->willReturn('e');
+        /** @var SysFileReferenceLocalizedParentDeleted $subject */
+        $subject = $this->get(SysFileReferenceLocalizedParentDeleted::class);
+        $subject->handle($io->reveal(), HealthInterface::MODE_EXECUTE, '');
+        $io->warning(Argument::cetera())->shouldHaveBeenCalled();
+        $io->note(Argument::cetera())->shouldHaveBeenCalled();
+        $io->text(Argument::cetera())->shouldHaveBeenCalled();
+        $this->assertCSVDataSet(__DIR__ . '/../Fixtures/SysFileReferenceLocalizedParentDeletedFixed.csv');
+    }
+}
---
+++ Tests/Functional/Health/SysFileReferenceLocalizedParentExistsTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lolli\Dbdoctor\Tests\Functional\Health;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Lolli\Dbdoctor\Health\HealthInterface;
+use Lolli\Dbdoctor\Health\SysFileReferenceLocalizedParentExists;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+class SysFileReferenceLocalizedParentExistsTest extends FunctionalTestCase
+{
+    use ProphecyTrait;
+
+    protected array $testExtensionsToLoad = [
+        'typo3conf/ext/dbdoctor',
+    ];
+
+    /**
+     * @test
+     */
+    public function fixBrokenRecords(): void
+    {
+        $this->importCSVDataSet(__DIR__ . '/../Fixtures/SysFileReferenceLocalizedParentExistsImport.csv');
+        $io = $this->prophesize(SymfonyStyle::class);
+        $io->ask(Argument::cetera())->willReturn('e');
+        /** @var SysFileReferenceLocalizedParentExists $subject */
+        $subject = $this->get(SysFileReferenceLocalizedParentExists::class);
+        $subject->handle($io->reveal(), HealthInterface::MODE_EXECUTE, '');
+        $io->warning(Argument::cetera())->shouldHaveBeenCalled();
+        $io->note(Argument::cetera())->shouldHaveBeenCalled();
+        $io->text(Argument::cetera())->shouldHaveBeenCalled();
+        $this->assertCSVDataSet(__DIR__ . '/../Fixtures/SysFileReferenceLocalizedParentExistsFixed.csv');
+    }
+}
