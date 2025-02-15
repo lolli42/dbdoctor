0cc62b9ac71c57d1789bbcb201552eb433ca28ac
Christian Kuhn <lolli@schwarzbu.ch>
2025-02-15 18:37:57 +0100

    [TASK] Remove further TYPO3 v11 compat layers

--- Classes/Renderer/RecordsRenderer.php
+++ Classes/Renderer/RecordsRenderer.php
@@ -92,16 +92,7 @@ final class RecordsRenderer
                 && isset($row['uid_foreign']) && isset($row['tablenames'])
             ) {
                 // Maybe make this more generic, we 'll see.
-                if (isset($row['table_local'])) {
-                    // v11 compat layer which still has table_local field in sys_file_reference
-                    // @todo: Remove when v11 compat is dropped from extension
-                    $row['table_local'] = $this->resolveRelationTable((string)$row['table_local']);
-                    if ($this->tableHelper->tableExistsInDatabase((string)$row['table_local'])) {
-                        $row['uid_local'] = $this->resolveRelation((string)$row['table_local'], (int)($row['uid_local']));
-                    }
-                } else {
-                    $row['uid_local'] = $this->resolveRelation('sys_file', (int)($row['uid_local']));
-                }
+                $row['uid_local'] = $this->resolveRelation('sys_file', (int)($row['uid_local']));
                 $row['tablenames'] = $this->resolveRelationTable((string)$row['tablenames']);
                 if ($this->tableHelper->tableExistsInDatabase((string)$row['tablenames'])) {
                     $row['uid_foreign'] = $this->resolveRelation((string)$row['tablenames'], (int)($row['uid_foreign']));
